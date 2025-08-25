3b6813d34cd3c1c36d127e76d15debdcdb4466c7
Jonathan Amaral <jonathansamael@live.com>
2025-08-25 17:29:20 -0300

    "update: new readme"

--- lib/app/core/services/api_service.dart
+++ lib/app/core/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   
-  static const String _baseUrl = 'https://wa-sud-adcondo-dev.azurewebsites.net/api';
+  static const String _baseUrl = '';
 
   final http.Client _client;
 
